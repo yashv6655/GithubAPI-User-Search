c76776d11e6480570f2deb2d2c56b1e35a85d968
Yash Velagapudi <yashv6655@gmail.com>
2020-10-02 15:25:05 -0700

    Initial Commit

---
+++ src/context/context.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
+import { createContext } from "react";
+
+const rootUrl = "https://api.github.com";
+
+const GithubContext = createContext();
+
+const GithubProvider = ({ children }) => {
+  return (
+    <GithubContext.Provider value={"hello"}>{children}</GithubContext.Provider>
+  );
+};
+
+export { GithubContext, GithubProvider };
