a3903dc14927a3a370d2483f5f85bbab3f36be5e
Yash Velagapudi <yashv6655@gmail.com>
2020-10-10 19:07:55 -0700

    Added Pie Chart

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,81 @@
-import React from 'react';
+// STEP 1 - Include Dependencies
+// Include react
+import React from "react";
 
-const Pie3D = () => {
-  return <div>chart</div>;
+// Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
+
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Chart from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+// STEP 2 - Chart Data
+const chartData = [
+  {
+    label: "Venezuela",
+    value: "344",
+  },
+  {
+    label: "Saudi",
+    value: "260",
+  },
+  {
+    label: "Canada",
+    value: "180",
+  },
+  {
+    label: "Iran",
+    value: "140",
+  },
+  {
+    label: "Russia",
+    value: "115",
+  },
+  {
+    label: "UAE",
+    value: "100",
+  },
+  {
+    label: "US",
+    value: "30",
+  },
+  {
+    label: "China",
+    value: "30",
+  },
+];
+
+// STEP 3 - Creating the JSON object to store the chart configurations
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "pie3d", // The chart type
+    width: "400", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Languages",
+        theme: "fusion",
+        decimals: 0,
+        pieRadius: "43%",
+        paletteColors:
+          "#a3586d, #5c4a72, #f3b05a, #f4874b, #f46a4e, #a7414a, #282726, #f18904, #563838, #192e5b, #1d65a6, #72a2c0, #00743f, #f2a104, #6465a5, #6975a6, #f28a30, #f05837",
+      },
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Pie3D;
+export default ChartComponent;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,8 +4,37 @@ import { GithubContext } from "../context/context";
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = useContext(GithubContext);
+  const data = [
+    {
+      label: "HTML",
+      value: 40,
+    },
+    {
+      label: "CSS",
+      value: 20,
+    },
+    {
+      label: "Javascript",
+      value: 30,
+    },
+    {
+      label: "C++",
+      value: 5,
+    },
+    {
+      label: "Java",
+      value: 5,
+    },
+  ];
   console.log(repos);
-  return <ExampleChart />;
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        {/* <ExampleChart /> */}
+        <Pie3D data={data} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
