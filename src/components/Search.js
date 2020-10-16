10c184be46b3c99dfa990f1a1f12697f719c5c30
Yash Velagapudi <yashv6655@gmail.com>
2020-10-15 17:17:36 -0700

    Created the Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,36 @@
-import React from 'react';
-import styled from 'styled-components';
-import { MdSearch } from 'react-icons/md';
-import { GithubContext } from '../context/context';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { MdSearch } from "react-icons/md";
+import { GithubContext } from "../context/context";
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user, setUser] = useState("");
+  // get global context
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+    if (user) {
+      setUser("");
+    }
+  };
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input
+              type="text"
+              placeholder="Github Username"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+            <button type="submit">Search</button>
+          </div>
+        </form>
+        <h3>Requests Left: 60/60</h3>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
