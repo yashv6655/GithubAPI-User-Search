d7dbdf8d2a125d01d49115d273869a4e6fae38ab
Yash Velagapudi <yashv6655@gmail.com>
2020-10-15 17:35:10 -0700

    Created Github API call for limit value

--- src/context/context.js
+++ src/context/context.js
@@ -13,8 +13,32 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+  //requests and loading
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // check rate
+  const checkRequests = () => {
+    axios
+      .get(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+
+        setRequests(remaining);
+
+        if (remaining === 0) {
+          //throw error
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(checkRequests, []);
+
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider value={{ githubUser, repos, followers, requests }}>
       {children}
     </GithubContext.Provider>
   );
