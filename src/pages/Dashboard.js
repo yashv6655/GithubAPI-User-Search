e303b66c40f7d8f95124054bb4aa14233bb31cb0
Yash Velagapudi <yashv6655@gmail.com>
2020-10-17 17:11:14 -0700

    Added loading conditional

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,17 +1,23 @@
-import React from "react";
-import { Info, Repos, User, Search, Navbar } from "../components";
+import React, {useContext} from "react";
+import {Info, Repos, User, Search, Navbar} from "../components";
 import loadingImage from "../images/preloader.gif";
-import { GithubContext } from "../context/context";
+import {GithubContext} from "../context/context";
 const Dashboard = () => {
-  return (
-    <main>
-      <Navbar />
-      <Search />
-      <Info />
-      <User />
-      <Repos />
+  const {isLoading} = useContext(GithubContext)
+  if (isLoading) {
+    return <main>
+      <Navbar/>
+      <Search/>
+      <img src={loadingImage} className="loading-img" alt="Loading..."/>
     </main>
-  );
+  }
+  return (<main>
+    <Navbar/>
+    <Search/>
+    <Info/>
+    <User/>
+    <Repos/>
+  </main>);
 };
 
 export default Dashboard;
