6b4e3cc14bc0097d9a861d72456ba498a97fe333
Yash Velagapudi <yashv6655@gmail.com>
2020-10-17 17:12:19 -0700

    Dynamic loading logic

--- src/context/context.js
+++ src/context/context.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import mockUser from "./mockData.js/mockUser";
 import mockRepos from "./mockData.js/mockRepos";
 import mockFollowers from "./mockData.js/mockFollowers";
 import axios from "axios";
-import { createContext } from "react";
+import {createContext} from "react";
 
 const rootUrl = "https://api.github.com";
 
 const GithubContext = createContext();
 
-const GithubProvider = ({ children }) => {
+const GithubProvider = ({children}) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
   //requests, loading & error
   const [requests, setRequests] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState({ show: false, msg: "" });
+  const [error, setError] = useState({show: false, msg: ""});
 
   const searchGithubUser = async (user) => {
-    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
-      console.log(err)
-    );
+    toggleError();
+    setIsLoading(true)
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) => console.log(err));
 
     console.log(response);
 
@@ -30,45 +30,44 @@ const GithubProvider = ({ children }) => {
     } else {
       toggleError(true, "User does not exist.");
     }
+    checkRequests()
+    setIsLoading(false)
   };
 
   // check rate
   const checkRequests = () => {
-    axios
-      .get(`${rootUrl}/rate_limit`)
-      .then(({ data }) => {
-        let {
-          rate: { remaining },
-        } = data;
-        setRequests(remaining);
+    axios.get(`${rootUrl}/rate_limit`).then(({data}) => {
+      let {rate: {
+          remaining
+        }} = data;
+      setRequests(remaining);
 
-        if (remaining === 0) {
-          toggleError(true, "Can't fetch user. You are out of requests.");
-        }
-      })
-      .catch((err) => console.log(err));
+      if (remaining === 0) {
+        toggleError(true, "Can't fetch user. You are out of requests.");
+      }
+    }).catch((err) => console.log(err));
   };
 
   function toggleError(show = false, msg = "") {
-    setError({ show, msg });
+    setError({show, msg});
   }
 
   useEffect(checkRequests, []);
 
-  return (
-    <GithubContext.Provider
-      value={{
-        githubUser,
-        repos,
-        followers,
-        requests,
-        error,
-        searchGithubUser,
-      }}
-    >
-      {children}
-    </GithubContext.Provider>
-  );
+  return (<GithubContext.Provider value={{
+      githubUser,
+      repos,
+      followers,
+      requests,
+      error,
+      searchGithubUser,
+      isLoading
+    }}>
+    {children}
+  </GithubContext.Provider>);
 };
 
-export { GithubContext, GithubProvider };
+export {
+  GithubContext,
+  GithubProvider
+};
