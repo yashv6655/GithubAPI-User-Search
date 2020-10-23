a884cbb340de0e2621477a566c912ed9dc511f29
Yash Velagapudi <yashv6655@gmail.com>
2020-10-22 19:36:55 -0700

    Wrapped App in Auth

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
-
+// dev-dv59zo09.us.auth0.com
+// 0mWGafL0uaNMfMbUOEH6vvNZJdl0CMyC
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-dv59zo09.us.auth0.com"
+      clientId="0mWGafL0uaNMfMbUOEH6vvNZJdl0CMyC"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
