da876b624c3fc4fecb4ad0632b59a8be84f34d49
Yash Velagapudi <yashv6655@gmail.com>
2020-10-23 21:10:24 -0700

    Removed console.log lines

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
-console.log(process.env.REACT_APP_DOMAIN_KEY, process.env.REACT_APP_CLIENT_ID);
+
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
